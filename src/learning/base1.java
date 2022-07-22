e3446f25445da90b3716e2c234fa5ae16221a911
qw163 <123345>
2022-07-22 17:30:58 +0800

    999

--- src/learning/base1.java
+++ src/learning/base1.java
@@ -1,12 +1,55 @@
 package learning;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class base1 {
     public static void main(String[] args) {
-        int a=2,b=4;
-        System.out.println(a>b ? a:b);
+
+//        System.out.println("请输入");
+//        Scanner amount= new Scanner(System.in);
+//        int ip=amount.nextInt();
+//        if(ip>10){
+//            System.out.println("very good");
+//        }
+//        else {System.out.println("NONONO");}
+//
+//    //if(9>4)
+//       // System.out.println("@22");
+//        boolean flag= false;
+//        if(flag){     //直接把变量写在里面
+//            System.out.println("falg");
+//        System.out.println("请输入1——100票号");
+//        Scanner put=new Scanner(System.in);
+//        int flag=put.nextInt();
+//        if(flag%2==0){
+//            System.out.println(flag+"为左侧");
+//        }else {
+//            System.out.println(flag+"为右边");
+//        }
+//        ArrayList amount= new ArrayList<>();
+
+//        System.out.println("请输入");
+//        Scanner input= new Scanner(System.in);
+//        switch (input.nextInt()){
+//            case 1 -> System.out.println("机票查询");
+//            case 2 -> System.out.println("机票预定");
+//            case 3 -> System.out.println("机票改签");
+//            case 4 -> System.out.println("退出服务");
+//            default->
+//                System.out.println("exit");
+
+        for (int i = 1; i <=100 ; i++) {
+            if(i%7==0){
+                System.out.println("过");
+        }else {
+                System.out.println(i);
+            }
 
 
+
+        }
     }
+
+
 }
