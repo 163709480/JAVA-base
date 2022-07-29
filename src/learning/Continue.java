993337d06d928ed3d2ef337c83ccbde14e0562be
qw163 <123345>
2022-07-29 08:57:53 +0800

    9994

--- src/learning/Continue.java
+++ src/learning/Continue.java
@@ -1,5 +1,6 @@
 package learning;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -14,12 +15,100 @@ public class Continue {
 //        double [] height= {12.4,45.3};
 //        System.out.println(height);
 
-        int [] arr= {1,2,3,4,5};
-        System.out.println(arr[0]);
+//        int [] arr= {1,2,3,4,5,6,7,8,9,10};
+//        int sum=0;
+//        int count=0;
+//        for (int i = 0; i < arr.length; i++) {
+//            if (arr[i] % 2  == 0) {
+//               sum=arr[i]/2;
+//                System.out.println(arr[i]+"偶数"+sum);
+//            }else if(arr[i]%2!=0){
+//                System.out.println("奇数"+arr[i]*2);
+//
+//            }
+//            }
+//
+//            System.out.println(count);
+
+//            String[] arr= new String[50];
+//            arr[0]="e";
+//
+//            int [] a=new int[3];
+//        System.out.println(arr[0]);
+//        System.out.println(arr[1]);
+//        System.out.println(arr[2]);
+//        int c=0;
+//
+//        int[] arr = {33, 100, 99, 44, 55};
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < 5; j++) {
+//                if(arr[i]>arr[j]){
+//
+//                 c=arr[i];
+//                }else if(arr[i]<arr[j]){
+//                    arr[i]=arr[j];
+//                }
+//
+//            }
+//
+//        }
+        //求和统计个数
+//        int [] arrge=new int[10];
+//        int sum=0;
+//        int average=0;
+//        int count=0;
+//        for (int i = 0; i < 10; i++) {
+//            Random ac= new Random();
+//            arrge[i]=ac.nextInt(100);
+//            System.out.println(arrge[i]);
+//            sum=sum+arrge[i];
+//
+//
+//        }
+//        int ave=sum/arrge.length;
+//        System.out.println("所有数据的总和为"+sum);
+//        System.out.println("所有数据的平均数"+ave);
+//        for (int i = 0; i < 10; i++) {
+//            if(ave>arrge[i]){
+//                count++;
+//
+//            }
+//
+//
+//        }
+//        System.out.println(count);
+//        System.out.println(arrge.length);
 
 
+//        System.out.println("22");
+//        int []arrge={1,2,3,4,5,};
+//        int []  ar= new int[5];
+//        int extra=0;
+//        for (int i = 0,j= arrge.length-1;i < j; i++,j--) {
+//            int tem=arrge[i];
+//            arrge[i]=arrge[j];
+//            arrge[j]=tem;
+//
+//
+//
+//
+//
+//        }
+//        for (int i = 0; i < arrge.length; i++) {
+//            System.out.println(arrge[i]);
+//
+//        }
 
+        int [] arge={1,2,3,4,5};
+                Random ro= new Random();
 
+        for (int i = 0; i < 5; i++) {
+            int c= ro.nextInt(5);
+            arge[i]=arge[c];
+            System.out.println(arge[i]);
+
+        }
+     }
 
 
     }
@@ -28,5 +117,5 @@ public class Continue {
 
 
 
-    }
+
 
