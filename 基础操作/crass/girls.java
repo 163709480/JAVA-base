a4b80bd79d9c83f6426c99879c188370110f6b03
qw163 <123345>
2022-09-01 11:31:35 +0800

    9994rrrr

---
+++ 基础操作/crass/girls.java
@@ -0,0 +1,54 @@
+package crass;
+
+public class girls {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public girls() {
+    }
+
+    private String name;
+
+    public girls(String name, int age, String gender, String hobby) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.hobby = hobby;
+    }
+
+    private int age;
+   private String gender;
+
+   private String hobby;
+
+
+}
